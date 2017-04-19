2ecf4654503c1a747bdae437dc7724eb0b7bef41
Carlos D. Arriaga Conde <cd-ac@hotmail.com>
2017-04-19 00:01:18 +0000

    Add validations to Client and Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,8 @@
 class Order < ActiveRecord::Base
     belongs_to :client
+    
+    validates :t_cost, presence: true
+    validates :payment, presence: true
+    validates :date, presence: true
+    
 end
