68fc95a5b5fd30dbd9394f8b4972abde63fedd4a
Carlos <cd-ac@hotmail.com>
2017-04-25 05:16:10 +0000

    modified orders_controller & orders/new. Sending product ids.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,11 @@
 class OrdersController < ApplicationController
     
     def new
-        
+        @order = Order.new
+        @products = Product.all
     end
     
+    def create
+        render plain: params[:order].inspect
+    end
 end
